9630e97113554c9317f8a8e8c6ff94d29cf40ac2
Sergio Tanaka <sergio.tanaka@pipefy-dev29.pipefy.local>
2019-07-10 09:16:30 -0300

    creation of kdash :P

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+    "fmt"
+    "regexp"
+    "strings"
+    "os/exec"
+    "runtime"
+    "log"
+    "github.com/atotto/clipboard"
+)
+
+func main() {
+
+    cmdName := "kubectl"
+    cmdArgs := []string{"config", "current-context"}
+    out, err := exec.Command(cmdName, cmdArgs...).Output()
+
+    if err != nil {
+        fmt.Printf("%s", err)
+    }
+
+    output := string(out[:])
+    output = strings.TrimSpace(output) //context
+    fmt.Println("Using context", output)
+
+    fmt.Println("Retrieving token...")
+    cmdName = "kubectl"
+    cmdArgs = []string{"config", "view", "--minify"}
+    out, err = exec.Command(cmdName, cmdArgs...).Output()
+
+    if err != nil {
+        fmt.Printf("%s", err)
+    }
+
+    output = string(out[:])
+    output = strings.TrimSpace(output)
+
+    re := regexp.MustCompile(`password.*`)
+    matches := re.FindStringSubmatch(output)
+
+    password_array := strings.Join(matches, ", ")
+    password := strings.Trim(password_array, "password: ") //password
+
+    fmt.Println("Sending token to clipboard...")
+    clipboard.WriteAll(password) //clipboard
+
+    fmt.Println("Opening your browser... \nPress Ctrl + C to finish!! :P")
+    url := "http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/"
+    openbrowser(url)
+
+    cmdName = "kubectl"
+    cmdArgs = []string{"proxy"}
+    out, err = exec.Command(cmdName, cmdArgs...).Output()
+
+    if err != nil {
+        fmt.Printf("%s", err)
+    }
+
+    output = string(out[:])
+    output = strings.TrimSpace(output) //context
+
+}
+
+func openbrowser(url string) {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
