593cb962688ea81b0802b495d71cd5f28b4edaba
Sergio Tanaka <sergio.tanaka@pipefy-dev29.pipefy.local>
2019-07-10 22:10:30 -0300

    func to execute commands

--- main.go
+++ main.go
@@ -12,53 +12,43 @@ import (
 
 func main() {
 
-    cmdName := "kubectl"
-    cmdArgs := []string{"config", "current-context"}
-    out, err := exec.Command(cmdName, cmdArgs...).Output()
+    getcontext := executecmd("kubectl", "config", "current-context")
+    fmt.Println("Using context", getcontext)
 
-    if err != nil {
-        fmt.Printf("%s", err)
-    }
-
-    output := string(out[:])
-    output = strings.TrimSpace(output) //context
-    fmt.Println("Using context", output)
-
-    fmt.Println("Retrieving token...")
-    cmdName = "kubectl"
-    cmdArgs = []string{"config", "view", "--minify"}
-    out, err = exec.Command(cmdName, cmdArgs...).Output()
-
-    if err != nil {
-        fmt.Printf("%s", err)
-    }
-
-    output = string(out[:])
-    output = strings.TrimSpace(output)
+    fmt.Println("Retrieving token")
+    gettoken := executecmd("kubectl", "config", "view", "--minify")
 
     re := regexp.MustCompile(`password.*`)
-    matches := re.FindStringSubmatch(output)
+    matches := re.FindStringSubmatch(gettoken)
 
     password_array := strings.Join(matches, ", ")
-    password := strings.Trim(password_array, "password: ") //password
+    password := strings.Trim(password_array, "password: ")
 
     fmt.Println("Sending token to your clipboard...")
-    clipboard.WriteAll(password) //clipboard
+    clipboard.WriteAll(password)
 
     fmt.Println("Opening your browser... \nPress Ctrl + C to finish!! :P")
     url := "http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/"
     openbrowser(url)
 
-    cmdName = "kubectl"
-    cmdArgs = []string{"proxy"}
-    out, err = exec.Command(cmdName, cmdArgs...).Output()
-
-    if err != nil {
-        fmt.Printf("%s", err)
-    }
+    executecmd("kubectl", "proxy")
 
 }
 
+func executecmd(cmd string, args ...string) string {
+
+    out, err := exec.Command(cmd, args...).Output()
+
+    if err != nil {
+        fmt.Printf("%s", err)
+	panic(err)
+    }
+
+    output := string(out[:])
+    output = strings.TrimSpace(output) //context
+    return output
+
+}
 
 func openbrowser(url string) {
 	var err error
